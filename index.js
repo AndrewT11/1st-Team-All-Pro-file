09935fc60bc336940380f1e9d06c2955c1c3c1cf
Andrew Tran <tranandrew@gmail.com>
2021-10-11 10:00:41 -0500

    added basic questions framework from last hw for employee questions to generateHTML and writefile

--- index.js
+++ index.js
@@ -4,3 +4,84 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateHTML = require('./utils/generateHTML');
 
+//look up extends
+// class Intern extends Employee()
+// or something like that. Subclass notes. Maybe superman prototype example.
+
+//baseline framework from last hw
+// TODO: Create an array of questions for user input
+const questions = function () {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: ''
+        },
+        {
+            type: 'list',
+            name: '',
+            message: '',
+            choices: [
+                
+            ]
+        },
+        {
+            type: 'list',
+            name: 'color',
+            message: '',
+            choices: [
+                
+            ]
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: ''
+        },
+        {
+            type: 'input',
+            name: 'linkedin',
+            message: ''
+        },
+    ])    
+}
+
+// TODO: Create a function to write README file
+function writeToFile(answers) {
+    fs.writeFileSync('./newHTML/index.html', generateHTML(answers))
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    questions()
+    .then((answers) => writeToFile(answers))
+    .catch((err) => console.error(err));
+}
+
+// Function call to initialize app
+init();
