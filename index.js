56e8fef94a7c4bd2cb666bbc7f5a55dd39df2958
Andrew Tran <tranandrew@gmail.com>
2021-10-17 01:05:22 -0500

    creating instances of classes. trying new format.

--- index.js
+++ index.js
@@ -23,132 +23,191 @@ const questions = function () {
             choices: [ 
                 "Manager",
                 "Engineer",
-                "Intern",
-                "No more. We broke."
+                "Intern",        
             ]
-        } 
-    ]).then((answers) => {
-            console.log(answers)
-            if(answers.role === "Manager"){
-                return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'name',
-                        message: "What is the employee's name?"
-                    },
-                    {
-                        type: 'input',
-                        name: 'id',
-                        message: 'Please input the employee\'s id'
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: 'Please input the employee\'s email'
-                    },
-                    {
-                        type: 'input',
-                        name: 'office',
-                        message: "What is the manager's office number?"
-                    },
-                    {
-                        type: 'confirm',
-                        name: 'addTeamMember',
-                        message: "Would you like to add another team member?"
-                    }
-                ]) .then((answers) => {
-                        teamArr.push(answers);
-                        if(answers.addTeamMember === true)  {
-                            questions();
-                        }
-                })
-            } else if(answers.role === "Engineer") {
-                return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'name',
-                        message: "What is the employee's name?"
-                    },
-                    {
-                        type: 'input',
-                        name: 'id',
-                        message: 'Please input the employee\'s id'
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: 'Please input the employee\'s email'
-                    },
-                    {
-                        type: 'input',
-                        name: 'github',
-                        message: "What is engineer's Github id?"
+        }
+    //add if statements here instead.
+            .then((answers) => {
+                if(answers.role === "Manager") {
+                    return inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: "What is the manager's name?"
+                        },
+                        {
+                            type: 'input',
+                            name: 'id',
+                            message: 'Please input the manager\'s id'
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: 'Please input the employee\'s email'
+                        },
+                        {
+                            type: 'input',
+                            name: 'office',
+                            message: "Enter manager's office number?"
+                        },
+                    ]).then(function (answers) {
+                        const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
+                        teamArr.push(manager)
+                        addTeamMember()
+                    })
+                } else if (answers.role === "Engineer") {
+                    return inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: "What is the engineer's name?"
+                        },
+                        {
+                            type: 'input',
+                            name: 'id',
+                            message: 'Please input the engineer\'s id'
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: 'Please input the engineer\'s email'
+                        },
+                        {
+                            type: 'input',
+                            name: 'github',
+                            message: "Enter enginener's Github?"
+                        },
+                    ]) .then(function (answers) {
+                        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+                        teamArr.push(engineer)
+                        addTeamMember()
+                    })
+                } else if (answers.role === "Intern") {
+                    return inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: "What is the intern's name?"
+                        },
+                        {
+                            type: 'input',
+                            name: 'id',
+                            message: 'Please input the intern\'s id'
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: 'Please input the intern\'s email'
+                        },
+                        {
+                            type: 'input',
+                            name: 'school',
+                            message: "What school did the intern attend?"
+                        },
+                    ]) .then(function(answers) {
+                        const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+                        teamArr.push(intern)
+                        addTeamMember()
+                        ])
+                    })
+                }
+                }
+                
+            })
 
-                    },
-                    {
-                        type: 'confirm',
-                        name: 'addTeamMember',
-                        message: "Would you like to add another team member?"
-                    }                    
-                ]).then((answers) => {
-                    teamArr.push(answers);
-                    if(answers.addTeamMember === true)  {
-                        questions();
-                    }
-                })
-            } else if(answers.role === "Intern") {
-                return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'name',
-                        message: "What is the employee's name?"
-                    },
-                    {
-                        type: 'input',
-                        name: 'id',
-                        message: 'Please input the employee\'s id'
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: 'Please input the employee\'s email'
-                    },
-                    {
-                        type: 'input',
-                        name: 'school',
-                        message: 'What school did the intern attend?'
-                    },
-                    {
-                        type: 'confirm',
-                        name: 'addTeamMember',
-                        message: "Would you like to add another team member?"
-                    }
-                ]).then((answers) => {
-                    teamArr.push(answers);
-                    if(answers.addTeamMember === true)  {
-                        questions();
-                    }
-                })
-            }
-        }).then((answers) => {
-            console.log(teamArr);
-        });
-}
-    
-// // TODO: Create a function to write README file
-// function writeToFile(answers) {
-//     fs.writeFileSync('./newHTML/index.html', generateHTML(answers))}
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please input the employee\'s id'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please input the employee\'s email'
+        },
+    //     {
+    //         type: 'input',
+    //         name: 'office',
+    //         message: "What is the manager's office number?",
+    //         when: (answers) => answers.role === "Manager" 
+                
+            
+    //     },
+    //     {
+    //         type: 'input',
+    //         name: 'github',
+    //         message: "What is the engineer's Github?",
+    //         when: (answers) => answers.role === "Engineer"
+            
+    //     },
+    //     {
+    //         type: 'input',
+    //         name: 'school',
+    //         message: "What school did the intern attend?",
+    //         when: (answers) => answers.role === "Intern"
+            
+    //     },
+    //     {
+    //         type: 'confirm',
+    //         name: 'addTeamMember',
+    //         message: "Would you like to add another team member?",
+    //         default: false,
+    //     },
+    // ]).then((answers) => {
+    //     if(answers.role = "Manager") {
+    //         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    //         teamArr.unshift(manager);
+    //         teamArr[0].role = answers.role;
+    //         console.log(teamArr);
+    //     }
+    //     if(answers.role = "Engineer") {
+    //         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+    //         teamArr.unshift(engineer);
+    //         teamArr[0].role = answers.role;
+    //         console.log('engineer created');
+    //     }
+    //     if(answers.role = "Intern") {
+    //         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    //         teamArr.unshift(intern);
+    //         teamArr[0].role = answers.role;
+    //         console.log('intern created');
+    //     }      
+    //     if(answers.addTeamMember === true) {
+    //         console.log("saying yes to new teams");
+    //         questions()
+    //     } else {
+    //         console.log("Saying no to new teammates");
+    //         console.log(teamArr);
+    //         return;
+    //     } 
+    // }                     
+// };
+
+// function addTeamMember() {
+
 // }
 
-// TODO: Create a function to initialize app
-function init() {
-    questions()
-    // .then((answers) => writeToFile(answers))
-    // .catch((err) => console.error(err));
-}
 
-// Function call to initialize app
-init();
+// // // TODO: Create a function to write README file
+// function writeToFile(teamArr) {
+    
+//     fs.writeFileSync('./newHTML/index.html', generateHTML(teamArr))}
+// // }
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     questions()
+//     .then((teamArr) => writeToFile(teamArr))
+//     .catch((err) => console.error(err));
+// }
+
+// // Function call to initialize app
+// init();
 
 //document.createElement
 // document.appendChild
@@ -156,33 +215,3 @@ init();
 // document.insertAdjacentHTML
 // http://net-informations.com/js/iq/elements.htm reminder of creating HTML dynamically
 // https://www.iusmentis.com/technology/www/relativeurls/ Using and interpreting relative URKs
-
-    // .then((answers) => {
-            
-        //     // let x = 'example'
-        //     // teamArr[0].x = 'hello'
-        //     console.log(answers)
-        //     if (answers.role === "Manager") { 
-        //         return inquirer.prompt([
-        //             {
-        //                 type: 'input',
-        //                 name: 'office',
-        //                 message: "What is the manager's office number?"
-        //             },
-        //             {
-        //                 type: 'confirm',
-        //                 name: 'addTeamMember',
-        //                 message: "Would you like to add another team member?"
-        //             }
-                    
-        //         ]).then((answers) => {
-        //             console.log(answers)
-        //             teamArr.push(answers)
-        //             console.log(teamArr)
-        //             if(answers.addTeamMember === 'Yes') {
-        //                 questions();
-        //             } else {
-        //                 return;
-        //             }
-                    
-                 //what to do ere. If another team member, want to questions() again. .then(answers). Where does this data go? Send info to make manager card. collect info from all employees, push into array, then generateHTML() generate items in the array on the screen.
--- trash.js
+++ trash.js
@@ -1,3 +1,222 @@
+]).then((answers) => {
+    teamArr.unshift(answers)
+    console.log(answers)
+    if(answers.role === "Manager"){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the employee's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please input the employee\'s id'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please input the employee\'s email'
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: "What is the manager's office number?",
+                when: (role) => role === "Manager"
+                
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the manager's office number?",
+                when: (role) => role === "Engineer"
+                
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the manager's office number?",
+                when: (role) => role === "School"
+                
+            },
+            {
+                type: 'confirm',
+                name: 'addTeamMember',
+                message: "Would you like to add another team member?"
+            }
+        ]) .then((answers) => {
+                teamArr.unshift(answers);
+                teamArr[0].role = answers;
+                if(answers.addTeamMember === true)  {
+                    questions();
+                }
+        })
+    } else if(answers.role === "Engineer") {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the employee's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please input the employee\'s id'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please input the employee\'s email'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is engineer's Github id?"
+
+            },
+            {
+                type: 'confirm',
+                name: 'addTeamMember',
+                message: "Would you like to add another team member?"
+            }                    
+        ]).then((answers) => {
+            teamArr.unshift(answers);
+            teamArr[0].role = answers.role;
+            if(answers.addTeamMember === true)  {
+                questions();
+            }
+        })
+    } else if(answers.role === "Intern") {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the employee's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please input the employee\'s id'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please input the employee\'s email'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school did the intern attend?'
+            },
+            {
+                type: 'confirm',
+                name: 'addTeamMember',
+                message: "Would you like to add another team member?"
+            }
+        ]).then((answers) => {
+            teamArr.unshift(answers);
+            teamArr[0].role = answers.role;
+            if(answers.addTeamMember === true)  {
+                questions();
+            }
+        })
+    }
+}).then((answers) => {
+    console.log(teamArr);
+});
+}
+
+
+// TODO: Create an array of questions for user input
+const questions = function () {
+    return inquirer.prompt([
+         {
+             type: 'list',
+             name: 'role',
+             message: 'What position does the employee hold?',
+             choices: [ 
+                 "Manager",
+                 "Engineer",
+                 "Intern",        
+             ]
+         },
+         {
+             type: 'input',
+             name: 'name',
+             message: "What is the employee's name?"
+         },
+         {
+             type: 'input',
+             name: 'id',
+             message: 'Please input the employee\'s id'
+         },
+         {
+             type: 'input',
+             name: 'email',
+             message: 'Please input the employee\'s email'
+         },
+         {
+             type: 'input',
+             name: 'office',
+             message: "What is the manager's office number?",
+             when: (answers) => answers.role === "Manager" 
+                 
+             
+         },
+         {
+             type: 'input',
+             name: 'github',
+             message: "What is the engineer's Github?",
+             when: (answers) => answers.role === "Engineer"
+             
+         },
+         {
+             type: 'input',
+             name: 'school',
+             message: "What school did the intern attend?",
+             when: (answers) => answers.role === "Intern"
+             
+         },
+         {
+             type: 'confirm',
+             name: 'addTeamMember',
+             message: "Would you like to add another team member?",
+             default: false,
+         },
+     ]).then(({answers}) => {
+         console.log(answers);
+         if(answers.addTeamMember === true) {
+             if(answers.role === "Manager") {
+                 const manager = new Manager(name, id, email, officeNumber);
+                 teamArr.push(manager);
+                 console.log("saying yes to new teams"); 
+                 console.log(teamArr);
+                 questions()
+             } if (answers.role === "Engineer") {
+                 const engineer = new Engineer(name, id, email, github);
+                 teamArr.push(engineer);
+                 console.log("saying yes to new teams"); 
+                 console.log(teamArr);
+                 questions();
+             } if (answers.role === "Intern") {
+                 const intern = new Intern(name, id, email, school);
+                 teamArr.push(intern);
+                 console.log("saying yes to new teams"); 
+                 console.log(teamArr);
+                 questions();
+             }
+         } else {
+             teamArr.unshift(answers);
+             console.log("Saying no to new teammates");
+             console.log(teamArr);
+             return;
+         } 
+     }                 
+ )};
+ 
+ questions();
+
+
 const questions = function () {
     return inquirer.prompt([
          {
@@ -106,3 +325,4 @@ const questions = function () {
  
  // Function call to initialize app
  init();
+ 
