ad16adaa92b4950525a2aebd782bc13901c7da2b
Andrew Tran <tranandrew@gmail.com>
2021-10-17 14:04:39 -0500

    object reference change

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -26,7 +26,7 @@ function generateHTML(teamArr) {
                 <div class="card employee-card" col-3>
                     <div class="card-header">
                         <h2 class="card-title">${this.name}</h2>
-                        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{{ role }}</h3>
+                        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${this.role}</h3>
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
@@ -47,7 +47,7 @@ function generateHTML(teamArr) {
                     <div class="card-body">
                         <ul class="list-group">
                             <li class="list-group-item">ID: ${this.id}</li>
-                            <li class="list-group-item">Email: <a href="mailto:{{ email }}">${this.email}</a></li>
+                            <li class="list-group-item">Email: <a href="mailto:${this.email}">${this.email}</a></li>
                             <li class="list-group-item">School: ${this.school}}</li>
                         </ul>
                     </div>
