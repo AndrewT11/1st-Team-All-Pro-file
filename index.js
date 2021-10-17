081e190aef4581e267871e680a3e5b095c61b542
Andrew Tran <tranandrew@gmail.com>
2021-10-17 01:33:07 -0500

    questions completed. array prints out. instances print

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const questions = function () {
                 "Intern",        
             ]
         }
+   ])
     //add if statements here instead.
             .then((answers) => {
                 if(answers.role === "Manager") {
@@ -53,6 +54,7 @@ const questions = function () {
                     ]).then(function (answers) {
                         const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
                         teamArr.push(manager)
+                        console.log('manager created')
                         addTeamMember()
                     })
                 } else if (answers.role === "Engineer") {
@@ -80,6 +82,7 @@ const questions = function () {
                     ]) .then(function (answers) {
                         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
                         teamArr.push(engineer)
+                        console.log('engineer created')
                         addTeamMember()
                     })
                 } else if (answers.role === "Intern") {
@@ -107,111 +110,45 @@ const questions = function () {
                     ]) .then(function(answers) {
                         const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
                         teamArr.push(intern)
+                        console.log('intern created')
                         addTeamMember()
-                        ])
-                    })
+                    })              
                 }
-                }
-                
-            })
-
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is the employee's name?"
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Please input the employee\'s id'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Please input the employee\'s email'
-        },
-    //     {
-    //         type: 'input',
-    //         name: 'office',
-    //         message: "What is the manager's office number?",
-    //         when: (answers) => answers.role === "Manager" 
-                
-            
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'github',
-    //         message: "What is the engineer's Github?",
-    //         when: (answers) => answers.role === "Engineer"
-            
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'school',
-    //         message: "What school did the intern attend?",
-    //         when: (answers) => answers.role === "Intern"
-            
-    //     },
-    //     {
-    //         type: 'confirm',
-    //         name: 'addTeamMember',
-    //         message: "Would you like to add another team member?",
-    //         default: false,
-    //     },
-    // ]).then((answers) => {
-    //     if(answers.role = "Manager") {
-    //         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-    //         teamArr.unshift(manager);
-    //         teamArr[0].role = answers.role;
-    //         console.log(teamArr);
-    //     }
-    //     if(answers.role = "Engineer") {
-    //         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
-    //         teamArr.unshift(engineer);
-    //         teamArr[0].role = answers.role;
-    //         console.log('engineer created');
-    //     }
-    //     if(answers.role = "Intern") {
-    //         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-    //         teamArr.unshift(intern);
-    //         teamArr[0].role = answers.role;
-    //         console.log('intern created');
-    //     }      
-    //     if(answers.addTeamMember === true) {
-    //         console.log("saying yes to new teams");
-    //         questions()
-    //     } else {
-    //         console.log("Saying no to new teammates");
-    //         console.log(teamArr);
-    //         return;
-    //     } 
-    // }                     
-// };
-
-// function addTeamMember() {
-
-// }
-
-
-// // // TODO: Create a function to write README file
-// function writeToFile(teamArr) {
+        })
     
-//     fs.writeFileSync('./newHTML/index.html', generateHTML(teamArr))}
-// // }
+}
 
-// // TODO: Create a function to initialize app
-// function init() {
-//     questions()
-//     .then((teamArr) => writeToFile(teamArr))
-//     .catch((err) => console.error(err));
+const addTeamMember = function () {
+    return inquirer.prompt([
+        {
+            name: "teamMember",
+            message: "Are there any other team members?",
+            type: "confirm",            
+        }
+    ]).then((answers) => {
+        if(answers.teamMember === true){
+            questions();
+        } else {
+            console.log(teamArr)
+            generateHTML(answers)
+        }
+    })
+}
+
+// // TODO: Create a function to write README file
+function writeToFile(teamArr) {
+    
+    fs.writeFileSync('./newHTML/index.html', generateHTML(teamArr))}
 // }
 
-// // Function call to initialize app
-// init();
+// TODO: Create a function to initialize app
+function init() {
+    questions()
+    .then((teamArr) => writeToFile(teamArr))
+    .catch((err) => console.error(err));
+}
 
-//document.createElement
-// document.appendChild
-// document.insertBefore
-// document.insertAdjacentHTML
-// http://net-informations.com/js/iq/elements.htm reminder of creating HTML dynamically
-// https://www.iusmentis.com/technology/www/relativeurls/ Using and interpreting relative URKs
+// Function call to initialize app
+init();
+
+
