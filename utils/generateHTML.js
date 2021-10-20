a4bdc6a45aae1db1166153889f6f8d8d2d1c9b2b
Andrew Tran <tranandrew@gmail.com>
2021-10-20 01:27:19 -0500

    did not change actual html. changed generated index.html. redoing css. bugged

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -2,10 +2,10 @@ const renderArr = [];
 
 const generateManager = (manager) => {
     return `
-    <div class="card employee-card col-3">
+    <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.name}</h2>
-            <h3 class="card-title"><i <i class="fas fa-user-tie"></i>${manager.role}</h3>
+            <h3 class="card-title"><i class="fas fa-space-shuttle"></i> ${manager.role}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
@@ -20,10 +20,10 @@ const generateManager = (manager) => {
 
 const generateEngineer = (engineer) => {
     return `                
-    <div class="card employee-card col-3">
+    <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${engineer.name}</h2>
-            <h3 class="card-title"><i class="far fa-star"></i>${engineer.role}</h3>
+            <h3 class="card-title"><i class="fas fa-hat-wizard"></i> ${engineer.role}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
@@ -39,16 +39,16 @@ const generateEngineer = (engineer) => {
 const generateIntern = (intern) => {
     console.log("intern inside")
     return `  
-            <div class="card employee-card col-3">
+            <div class="card employee-card">
                 <div class="card-header">
                     <h2 class="card-title">${intern.name}</h2>
-                    <h3 class="card-title"><i <i class="far fa-star"></i>${intern.role}</h3>
+                    <h3 class="card-title"><i class="fas fa-user-ninja"></i> ${intern.role}</h3>
                 </div>
                 <div class="card-body">
                     <ul class="list-group">
                         <li class="list-group-item">ID: ${intern.id}</li>
                         <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
-                        <li class="list-group-item">School: ${intern.school}}</li>
+                        <li class="list-group-item">School: ${intern.school}</li>
                     </ul>
                 </div>
             </div>
@@ -91,23 +91,23 @@ return `
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
     <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
       integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-      crossorigin="anonymous"
+      crossorigin="anonymous"/>
       <link rel="stylesheet" href="style.css"/>
     />
 
-    <title>The Squawd</title>
+    <title>The Jungle Squawd</title>
 </head>
 <body>
-<section class="container col 11">
-<div class = "col 10"> Team Profile Generator</div>
-
+<section class="container">
+<div class = "header"> The Jungle Squad</div>
+    <div class="card-container">
     ${renderTeamArr(teamArr)}
-
+    </div>
 </section>
 </body>
 </html>`
